10fb9f135dd4d26c4f3af3f4b77dd1203f4f9fef
WASI AL SAFA <2103058@student.ruet.ac.bd>
2025-10-25 23:39:06 +0600

    connec4 in terminal kinda

--- connect4.py
+++ connect4.py
@@ -1,7 +1,52 @@
 import numpy as np
+ROW_COUNT=6
+COLUMN_COUNT=7
+
 
 def create_board():
-    board= np.ones((6,7))
+    board= np.zeros((6,7))
     return board
-board= create_board()
-print (board)
+
+def drop_piece(board,row,col,piece):
+    board[row][col] =piece
+
+def is_valid_location(board,col):
+    return board[5][col]==0
+
+def get_next_open_row(board,col):
+    for r in range(ROW_COUNT):
+        if(board[r][col]==0):
+            return r
+        
+def print_board(board):
+    print(np.flip(board,0))
+
+
+
+board=create_board()
+print(board)
+game_over=False
+turn=0
+
+while not game_over:
+    #ask for p1 input 
+    if turn==0:
+        col=int(input("player 1 make your move (0-6): "))
+        if is_valid_location(board,col):
+            row=get_next_open_row(board,col)
+            drop_piece(board,row,col,1)
+
+        
+
+    #ask for p2 input
+    else:
+        col=int(input("player 2 make your move (0-6): "))
+        if is_valid_location(board,col):
+            row=get_next_open_row(board,col)
+            drop_piece(board,row,col,2)
+
+    print_board(board)
+
+    turn+=1
+    turn=turn%2
+
